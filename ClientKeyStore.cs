4bdac94d6dabe495a3517ec14e6974cf15166252
Sylwester <sylwester@grabowscy.net>
2015-11-23 21:11:54 +0100

    added api

---
+++ ClientKeyStore.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Storage;
+
+namespace LgTv
+{
+    public class ClientKeyStore
+    {
+        private string ip;
+        public ClientKeyStore(string ip)
+        {
+            this.ip = ip;
+        }
+        public string GetClientKey()
+        {
+            return (string)ApplicationData.Current.LocalSettings.Values[ip + "ClientKey"];
+        }
+        public void SaveClientKey(string key)
+        {
+            ApplicationData.Current.LocalSettings.Values[ip + "ClientKey"] = key;
+        }
+    }
+}
---
+++ LgWebOsMouseService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LgTv
+{
+    public enum ButtonType
+    {
+        HOME,
+        BACK,
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        RED,
+        BLUE,
+        YELLOW,
+        GREEN
+    }
+
+    public class LgWebOsMouseService : IDisposable
+    {
+        private readonly LgTvApiCore _connection;
+        public LgWebOsMouseService(LgTvApiCore connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<bool> Connect(string uri)
+        {
+            var ctx = _connection.Connect(new Uri(uri), ignoreReceiver: true);
+            return (await ctx);
+        }
+        public void SendButton(int number)
+        {
+            _connection.SendMessageAsync($"type:button\nname:{number}\n\n");
+        }
+        public void SendButton(ButtonType bt)
+        {
+            _connection.SendMessageAsync($"type:button\nname:{bt.ToString()}\n\n");
+        }
+
+
+        public void Move(double dx, double dy, bool drag = false)
+        {
+            _connection.SendMessageAsync($"type:move\ndx:{dx}\ndy:{dy}\ndown:{(drag ? 1 : 0)}\n\n");
+        }
+
+        public void Scroll(double dx, double dy)
+        {
+            _connection.SendMessageAsync($"type:scroll\ndx:{dx}\ndy:{dy}\n\n");
+        }
+
+        public void Click()
+        {
+            _connection.SendMessageAsync("type:click\n\n");
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
